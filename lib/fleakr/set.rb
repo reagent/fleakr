bab413e9fb68179e9f19ac185c7563a88e627704
Patrick Reagan <patrick.reagan@viget.com>
2008-11-29 20:26:43 -0500

    Correct the traversal to get to the photoset collection

--- lib/fleakr/set.rb
+++ lib/fleakr/set.rb
@@ -6,7 +6,7 @@ module Fleakr
     def self.find_all_by_user_id(user_id)
       response = Request.new('photosets.getList', :user_id => user_id).send
       
-      (response.body/'photosets/photoset').map do |flickr_set|
+      (response.body/'rsp/photosets/photoset').map do |flickr_set|
         set = Set.new
         set.title = (flickr_set/'title').inner_text
         set.description = (flickr_set/'description').inner_text
