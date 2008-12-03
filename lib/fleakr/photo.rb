cb79a2eef0fcd6668dfb480f05706bdc4b08020d
Patrick Reagan <patrick.reagan@viget.com>
2008-12-02 23:54:41 -0500

    Implemented retrieval of image urls for photos

--- lib/fleakr/photo.rb
+++ lib/fleakr/photo.rb
@@ -5,6 +5,9 @@ module Fleakr
     
     flickr_attribute :title, :attribute => 'title'
     flickr_attribute :id, :attribute => 'id'
+    flickr_attribute :farm_id, :attribute => 'farm'
+    flickr_attribute :server_id, :attribute => 'server'
+    flickr_attribute :secret, :attribute => 'secret'
     
     def self.find_all_by_photoset_id(photoset_id)
       response = Request.with_response!('photosets.getPhotos', :photoset_id => photoset_id)
@@ -14,6 +17,30 @@ module Fleakr
     def self.find_all_by_user_id(user_id)
       response = Request.with_response!('people.getPublicPhotos', :user_id => user_id)
       (response.body/'rsp/photos/photo').map {|p| Photo.new(p) }
+    end
+    
+    def base_url
+      "http://farm#{self.farm_id}.static.flickr.com/#{self.server_id}/#{self.id}_#{self.secret}"
+    end
+    
+    def square
+      "#{self.base_url}_s.jpg"
+    end
+    
+    def thumbnail
+      "#{self.base_url}_t.jpg"
+    end
+    
+    def small
+      "#{self.base_url}_m.jpg"
+    end
+    
+    def medium
+      "#{self.base_url}.jpg"
+    end
+    
+    def large
+      "#{self.base_url}_b.jpg"
     end
     
   end
--- test/fleakr/photo_test.rb
+++ test/fleakr/photo_test.rb
@@ -16,11 +16,59 @@ module Fleakr
           @object = Photo.new(Hpricot.XML(read_fixture('people.getPublicPhotos')).at('rsp/photos/photo'))
         end
 
-        should_have_a_value_for :id    => '2924549350'
-        should_have_a_value_for :title => 'Photo #1'
+        should_have_a_value_for :id        => '2924549350'
+        should_have_a_value_for :title     => 'Photo #1'
+        should_have_a_value_for :farm_id   => '4'
+        should_have_a_value_for :server_id => '3250'
+        should_have_a_value_for :secret    => 'cbc1804258'
 
       end
     end
-    
+
+    context "in general" do
+
+      before do
+        @photo = Photo.new
+
+        @photo.stubs(:id).with().returns('1')
+        @photo.stubs(:farm_id).with().returns('2')
+        @photo.stubs(:server_id).with().returns('3')
+        @photo.stubs(:secret).with().returns('secret')
+      end
+
+      it "should know the base URL to retrieve images" do
+        @photo.base_url.should == "http://farm2.static.flickr.com/3/1_secret"
+      end
+
+    end
+
+    context "with a base URL" do
+
+      before do
+        @photo = Photo.new
+        @photo.stubs(:base_url).with().returns('url')
+      end
+
+      it "should know the URL for the square image" do
+        @photo.square.should == 'url_s.jpg'
+      end
+
+      it "should know the URL for the thumbnail image" do
+        @photo.thumbnail.should == 'url_t.jpg'
+      end
+
+      it "should know the URL for the small image" do
+        @photo.small.should == 'url_m.jpg'
+      end
+
+      it "should know the URL for the medium image" do
+        @photo.medium.should == 'url.jpg'
+      end
+
+      it "should know the URL for the large image" do
+        @photo.large.should == 'url_b.jpg'
+      end
+    end
   end
+
 end
