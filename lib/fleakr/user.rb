1a88c7d4de0783d6bb33649a1ceace8d66648402
Patrick Reagan <patrick.reagan@viget.com>
2008-12-02 20:56:09 -0500

    Allow User instance to retrieve additional information through the API

--- lib/fleakr/user.rb
+++ lib/fleakr/user.rb
@@ -1,28 +1,51 @@
 module Fleakr
   class User
-    
+
     include Fleakr::Object
-    
+
     flickr_attribute :id, :xpath => 'rsp/user', :attribute => 'nsid'
     flickr_attribute :username, :xpath => 'rsp/user/username'
-    
+    flickr_attribute :photos_url, :xpath => 'rsp/person/photosurl'
+    flickr_attribute :profile_url, :xpath => 'rsp/person/profileurl'
+    flickr_attribute :photos_count, :xpath => 'rsp/person/photos/count'
+    flickr_attribute :icon_server, :xpath => 'rsp/person', :attribute => 'iconserver'
+    flickr_attribute :icon_farm, :xpath => 'rsp/person', :attribute => 'iconfarm'
+
+    def icon_farm_with_load
+      retrieve_additional_information
+      icon_farm_without_load
+    end
+
     def self.find_by_username(username)
       response = Request.with_response!('people.findByUsername', :username => username)
       User.new(response.body)
     end
-    
+
     def self.find_by_email(email)
       response = Request.with_response!('people.findByEmail', :find_email => email)
       User.new(response.body)
     end
-    
+
+    def load_info
+      response = Request.with_response!('people.getInfo', :user_id => self.id)
+      self.populate_from(response.body)
+    end
+
+    def icon_url
+      if self.icon_server.to_i > 0
+        "http://farm#{self.icon_farm}.static.flickr.com/#{self.icon_server}/buddyicons/#{self.id}.jpg"
+      else
+        'http://www.flickr.com/images/buddyicon.jpg'
+      end
+    end
+
     def sets
       @set ||= Set.find_all_by_user_id(self.id)
     end
-    
+
     def groups
       @groups ||= Group.find_all_by_user_id(self.id)
     end
-    
+
   end
 end
--- test/fleakr/user_test.rb
+++ test/fleakr/user_test.rb
@@ -26,8 +26,8 @@ module Fleakr
     describe "An instance of User" do
 
       before do
-        response_body = Hpricot.XML(read_fixture('people.findByUsername'))
-        @user = User.new(response_body)
+        @user = User.new(Hpricot.XML(read_fixture('people.findByUsername')))
+        @user.populate_from(Hpricot.XML(read_fixture('people.getInfo')))
       end
 
       it "should have a value for :id" do
@@ -36,6 +36,26 @@ module Fleakr
       
       it "should have a value for :username" do
         @user.username.should == 'frootpantz'
+      end
+      
+      it "should have a value for :photos_url" do
+        @user.photos_url.should == 'http://www.flickr.com/photos/frootpantz/'
+      end
+      
+      it "should have a value for :profile_url" do
+        @user.profile_url.should == 'http://www.flickr.com/people/frootpantz/'
+      end
+      
+      it "should have a value for :photos_count" do
+        @user.photos_count.should == '3907'
+      end
+      
+      it "should have a value for :icon_server" do
+        @user.icon_server.should == '30'
+      end
+      
+      it "should have a value for :icon_farm" do
+        @user.icon_farm.should == '1'
       end
       
       it "should be able to retrieve the user's associated sets" do
@@ -64,6 +84,31 @@ module Fleakr
       it "should memoize the results returned for this user's group" do
         Group.expects(:find_all_by_user_id).once.returns([])
         2.times { @user.groups }
+      end
+      
+      it "should be able to retrieve additional information about the current user" do
+        response = mock_request_cycle :for => 'people.getInfo', :with => {:user_id => @user.id}
+        @user.expects(:populate_from).with(response.body)
+        
+        @user.load_info
+      end
+      
+      it "should be able to generate an icon URL when the :icon_server value is greater than zero" do
+        @user.stubs(:icon_server).with().returns('1')
+        @user.stubs(:icon_farm).with().returns('2')
+        @user.stubs(:id).with().returns('45')
+        
+        @user.icon_url.should == 'http://farm2.static.flickr.com/1/buddyicons/45.jpg'
+      end
+      
+      it "should return the default icon URL when the :icon_server value is zero" do
+        @user.stubs(:icon_server).with().returns('0')
+        @user.icon_url.should == 'http://www.flickr.com/images/buddyicon.jpg'
+      end
+      
+      it "should return the default icon URL when the :icon_server value is nil" do
+        @user.stubs(:icon_server).with().returns(nil)
+        @user.icon_url.should == 'http://www.flickr.com/images/buddyicon.jpg'
       end
       
     end
