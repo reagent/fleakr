55339f12933878f68127846e6d54e35d90c1fa41
Patrick Reagan <patrick.reagan@viget.com>
2008-12-17 00:56:14 -0500

    Initial implementation of authentication token

---
+++ lib/fleakr/objects/authentication_token.rb
@@ -0,0 +1,33 @@
+module Fleakr
+  module Objects # :nodoc:
+    
+    # = AuthenticationToken
+    #
+    # This class represents an authentication token used for API calls that 
+    # require authentication before they can be used
+    #
+    # == Attributes
+    #
+    # [value] The token value that is used in subsequent API calls
+    # [permissions] The permissions granted to this application (read / write / delete)
+    #
+    class AuthenticationToken
+      
+      include Fleakr::Support::Object
+      
+      flickr_attribute :value, :xpath => 'rsp/auth/token'
+      flickr_attribute :permissions, :xpath => 'rsp/auth/perms'
+      
+      # Retrieve a full authentication token from the supplied mini-token (e.g. 123-456-789)
+      #
+      def self.from_mini_token(token)
+        parameters = {:mini_token => token, :sign? => true}
+        response = Fleakr::Api::Request.with_response!('auth.getFullToken', parameters)
+        
+        self.new(response.body)
+      end
+      
+    end
+    
+  end
+end
---
+++ test/unit/fleakr/objects/authentication_token_test.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__) + '/../../../test_helper'
+
+module Fleakr::Objects
+  class AuthenticationTokenTest < Test::Unit::TestCase
+    
+    describe "The AuthenticationToken class" do
+      
+      it "should be able to create an instance from a mini_token" do
+        token = '123-123-123'
+        auth_token = stub()
+        
+        response = mock_request_cycle :for => 'auth.getFullToken', :with => {:mini_token => token, :sign? => true}
+        
+        AuthenticationToken.expects(:new).with(response.body).returns(auth_token)
+        
+        AuthenticationToken.from_mini_token(token).should == auth_token
+      end
+      
+    end
+    
+    describe "An instance of the AuthenticationToken class" do
+      
+      context "when populating from an XML document" do
+        
+        before do
+          @object = AuthenticationToken.new(Hpricot.XML(read_fixture('auth.getFullToken')))
+        end
+        
+        should_have_a_value_for :value       => 'abc-123'
+        should_have_a_value_for :permissions => 'delete'
+        
+      end
+      
+    end
+    
+  end
+end
