8d56dfed961ddd9ddb03fa0e4e2fcbed56053009
Patrick Reagan <patrick.reagan@viget.com>
2009-04-05 01:48:53 -0400

    Implement author and related tags for tag

--- lib/fleakr/objects/tag.rb
+++ lib/fleakr/objects/tag.rb
@@ -11,6 +11,19 @@ module Fleakr
       
       find_all :by_photo_id, :call => 'tags.getListPhoto', :path => 'photo/tags/tag'
       
+      def author
+        @author ||= User.find_by_id(author_id) unless author_id.nil?
+      end
+      
+      def related
+        response = Fleakr::Api::MethodRequest.with_response!('tags.getRelated', :tag => value)
+        (response.body/'rsp/tags/tag').map {|e| Tag.new(e) }
+      end
+      
+      def to_s
+        value
+      end
+      
     end
     
   end
--- test/unit/fleakr/objects/tag_test.rb
+++ test/unit/fleakr/objects/tag_test.rb
@@ -22,6 +22,60 @@ module Fleakr::Objects
         
       end
       
+      it "should have an author" do
+        user = stub()
+        
+        tag = Tag.new
+        tag.expects(:author_id).at_least_once.with().returns('1')
+        
+        
+        User.expects(:find_by_id).with('1').returns(user)
+        
+        tag.author.should == user
+      end
+      
+      it "should memoize the author data" do
+        tag = Tag.new
+        tag.expects(:author_id).at_least_once.with().returns('1')
+        
+        User.expects(:find_by_id).with('1').once.returns(stub())
+        
+        2.times { tag.author }
+      end
+      
+      it "should return nil for author if author_id is not present" do
+        tag = Tag.new
+        tag.expects(:author_id).with().returns(nil)
+        
+        tag.author.should be(nil)
+      end
+      
+      it "should have related tags" do
+        tag = Tag.new
+        tag.expects(:value).with().returns('foo')
+        
+        response = mock_request_cycle :for => 'tags.getRelated', :with => {:tag => 'foo'}
+
+
+        stubs = []
+        elements = (response.body/'rsp/tags/tag').map
+        
+        elements.each do |element|
+          stub = stub()
+          stubs << stub
+
+          Tag.expects(:new).with(element).returns(stub)
+        end
+        
+        tag.related.should == stubs
+      end
+      
+      it "should be able to generate a string representation of itself" do
+        tag = Tag.new
+        tag.expects(:value).with().returns('foo')
+        tag.to_s.should == 'foo'
+      end
+      
     end
     
   end
