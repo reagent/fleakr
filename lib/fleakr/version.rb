41978abb4768f22378da2f77eff997eed32155bb
Patrick Reagan <patrick.reagan@viget.com>
2009-10-25 21:20:02 -0400

    Bump to version 0.6.0

--- lib/fleakr/version.rb
+++ lib/fleakr/version.rb
@@ -2,8 +2,8 @@ module Fleakr
   module Version # :nodoc:
     
     MAJOR = 0
-    MINOR = 5
-    TINY  = 2
+    MINOR = 6
+    TINY  = 0
     
     def self.to_s
       [MAJOR, MINOR, TINY].join('.')
