14c5ac5bedc34fdb9fe0ae9443962ea3b2e424af
Patrick Reagan <reaganpr@gmail.com>
2010-08-05 18:22:54 -0600

    Clean up output when inspecting objects

--- lib/fleakr/support/object.rb
+++ lib/fleakr/support/object.rb
@@ -1,13 +1,13 @@
 module Fleakr
   module Support # :nodoc:all
     module Object
-    
+
       module ClassMethods
-      
+
         def attributes
           @attributes ||= []
         end
-      
+
         def flickr_attribute(*names_and_options)
           options = names_and_options.extract_options!
 
@@ -16,7 +16,7 @@ module Fleakr
             class_eval "attr_accessor :#{name}"
           end
         end
-      
+
         def has_many(*attributes)
           class_name = self.name
 
@@ -31,34 +31,34 @@ module Fleakr
             CODE
           end
         end
-      
+
         def find_all(condition, options)
           attribute    = options[:using].nil? ? condition.to_s.sub(/^by_/, '') : options[:using]
           target_class = options[:class_name].nil? ? self.name : "Fleakr::Objects::#{options[:class_name]}"
-        
+
           class_eval <<-CODE
             def self.find_all_#{condition}(value, options = {})
               options.merge!(:#{attribute} => value)
-              
+
               response = Fleakr::Api::MethodRequest.with_response!('#{options[:call]}', options)
               (response.body/'rsp/#{options[:path]}').map {|e| #{target_class}.new(e, options) }
             end
           CODE
         end
-      
+
         def find_one(condition, options)
           attribute = options[:using].nil? ? condition.to_s.sub(/^by_/, '') : options[:using]
-        
+
           class_eval <<-CODE
             def self.find_#{condition}(value, options = {})
               options.merge!(:#{attribute} => value)
-              
+
               response = Fleakr::Api::MethodRequest.with_response!('#{options[:call]}', options)
               #{self.name}.new(response.body, options)
             end
           CODE
         end
-        
+
         def scoped_search
           key = "#{self.name.demodulize.underscore.downcase}_id".to_sym
 
@@ -66,7 +66,7 @@ module Fleakr
             def search(*parameters)
               options = {:#{key} => self.id}
               options.merge!(self.authentication_options)
-              
+
               parameters << options
               Fleakr::Objects::Search.new(*parameters).results
             end
@@ -86,18 +86,18 @@ module Fleakr
             CODE
           end
         end
-      
+
       end
-    
+
       module InstanceMethods
-      
+
         attr_reader :document, :authentication_options
-      
+
         def initialize(document = nil, options = {})
           self.populate_from(document) unless document.nil?
           @authentication_options = options.extract!(:auth_token)
         end
-      
+
         def populate_from(document)
           @document = document
           self.class.attributes.each do |attribute|
@@ -105,14 +105,21 @@ module Fleakr
             self.send("#{attribute.name}=".to_sym, value) unless value.nil?
           end
         end
-      
+
+        def inspect
+          names      = instance_variables.reject {|n| n == '@document' }
+          attributes = names.map {|n| "#{n}=#{instance_variable_get(n).inspect}" }
+
+          "#<#{self.class} #{attributes.join(', ')}>"
+        end
+
       end
 
       def self.included(other)
         other.send(:extend, Fleakr::Support::Object::ClassMethods)
         other.send(:include, Fleakr::Support::Object::InstanceMethods)
       end
-      
+
     end
   end
 end
