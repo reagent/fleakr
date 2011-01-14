d1dca3ca63d693d9b900d0f62628e160b492aedf
Patrick Reagan <reaganpr@gmail.com>
2011-01-14 14:03:01 -0700

    Bump to 0.7.0

--- lib/fleakr/version.rb
+++ lib/fleakr/version.rb
@@ -1,13 +1,13 @@
 module Fleakr
   module Version # :nodoc:
-    
+
     MAJOR = 0
-    MINOR = 6
-    TINY  = 3
-    
+    MINOR = 7
+    TINY  = 0
+
     def self.to_s
       [MAJOR, MINOR, TINY].join('.')
     end
-    
+
   end
 end
